b1dc12f52eb5cddb1ee47b2b4adb963823c98d79
柠乐倒牙 <55522211+sy-sophie@users.noreply.github.com>
2024-11-18 23:13:35 +0800

    Update constants.js

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -1,6 +1,6 @@
 const abi = require('./Transaction.json');
 
-const contractAddress = "0x6b1B6DCe7Bdb5297d5a996ae67592345fE660f31";
+const contractAddress = "";
 const contractABI = abi.abi;
 
 module.exports = { contractAddress, contractABI };
